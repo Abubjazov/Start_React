64a0e12966746bbc565c159519705ea5443c39ec
Radik Abubjazov <abubjazov@mail.ru>
2021-10-25 05:59:52 +0300

    введение в React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
 
+const btnText = 'Print'
+
 function App() {
   return (
     <div className='App'>
-      
+      <h2 className = 'cls'>Text: {`${new Date()}`}</h2>
+      <input type={'text'} />
+      <button>{btnText}</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// const element = <h2>Hello World</h2>
+// const element2 = React.createElement('h2', {className: 'green'}, 'Hello World')
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 )
