81c195c53031804a2ca932c2888a0eb0c5aefeeb
Radik Abubjazov <abubjazov@mail.ru>
2021-10-28 08:14:55 +0300

    тема: state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import { Component } from 'react'
 import './App.css'
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname is {surname}</h1>
-      <a href={link} target="_blank" rel="noreferrer">My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      age: 27,
+      btnText: '+'
+    }
+  }
+
+  addAge = () => {
+    this.setState(state => ({
+        age: state.age + 1,
+        btnText: '?'
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props
+
+    return (
+      <div>
+        <h1>My name is {name}, surname is {surname}, age: {this.state.age}</h1>
+        <a href={link} target="_blank" rel="noreferrer">My profile</a>
+        <button onClick={this.addAge}>{this.state.btnText}</button>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      {/* <WhoAmI 
-        name="John"
-        surname="Smith"
-        link="https://getbootstrap.com/"/> */}
       <WhoAmI 
-        name={() => 'John'}
+        name="John"
         surname="Smith"
         link="https://getbootstrap.com/"/>
     </div>
