ecd14032f1121dbddbf2472019ebd1538b8b4183
Radik Abubjazov <abubjazov@mail.ru>
2021-11-04 21:11:16 +0300

    тема: react bootstrap

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BootstrapTest } from './BootstrapTest'
+
 ReactDOM.render(
   <StrictMode>
-    <App/>
+    <App />
+    <BootstrapTest />
   </StrictMode>,
   document.getElementById('root')
 )
