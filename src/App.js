3fc98a5f94bd9ccccdfa41bcc39a4d4dedc4df35
Radik Abubjazov <abubjazov@mail.ru>
2021-11-07 19:31:53 +0300

    тема: Portal

--- src/App.js
+++ src/App.js
@@ -1,47 +1,130 @@
-import React, {Component} from 'react'
-import {Container} from 'react-bootstrap'
-import './App.css'
+import React, {Component} from 'react';
+import ReactDOM  from 'react-dom';
+import {Container} from 'react-bootstrap';
+import './App.css';
 
 class Form extends Component {
-    // testRef = React.createRef()
+  state ={
+    advOpen: false
+  }
 
-    // componentDidMount() {
-    //   this.testRef.current.focus()
-    // }
+  handleClick = () => {
+    this.setState(({advOpen}) => ({
+      advOpen: !advOpen
+    }))
+  }
 
-    setInputRef = elem => {
-      this.testRef = elem
-    }
-
-    focusFirst = () => {
-      if (this.testRef) this.testRef.focus()
-    }
+  componentDidMount() {
+    setTimeout(this.handleClick, 3000)
+  }
 
     render() {
         return (
             <Container>
-                <form className="w-50 border mt-5 p-3 m-auto">
+                <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
+                style={{'overflow': 'hidden', 
+                        'position': 'relative'}}>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input ref={this.setInputRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea onFocus={this.focusFirst} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
+                    {
+                      this.state.advOpen ?
+                      <Portal>
+                        <Message />
+                      </Portal> : null
+                    }
+                    
                 </form>
             </Container>
         )
     }
 }
 
+const Portal = (props) => {
+  const node = document.createElement('div')
+  document.body.appendChild(node)
+
+  return ReactDOM.createPortal(props.children, node)
+}
+
+const Message = () => {
+  return (
+    <div 
+      style={{'width': '500px', 
+            'height': '150px', 
+            'backgroundColor': 'red', 
+            'position': 'absolute', 
+            'right': '0', 
+            'bottom': '0'}}>
+        Hello
+    </div>
+  )
+}
+
 function App() {
     return (
         <Form/>
-    )
+    );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react'
+// import {Container} from 'react-bootstrap'
+// import './App.css'
+
+// class Form extends Component {
+//     // testRef = React.createRef()
+
+//     // componentDidMount() {
+//     //   this.testRef.current.focus()
+//     // }
+
+//     setInputRef = elem => {
+//       this.testRef = elem
+//     }
+
+//     focusFirst = () => {
+//       if (this.testRef) this.testRef.focus()
+//     }
+
+//     render() {
+//         return (
+//             <Container>
+//                 <form className="w-50 border mt-5 p-3 m-auto">
+//                     <div className="mb-3">
+//                         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+//                         <input ref={this.setInputRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+//                     </div>
+//                     <div className="mb-3">
+//                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//                         <textarea onFocus={this.focusFirst} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+//                     </div>
+//                 </form>
+//             </Container>
+//         )
+//     }
+// }
+
+// function App() {
+//     return (
+//         <Form/>
+//     )
+// }
+
+// export default App
 
 
 
