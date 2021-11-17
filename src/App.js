83349cb10b12c790d2fe990347f9e8741de21319
Radik Abubjazov <abubjazov@mail.ru>
2021-11-17 20:45:54 +0300

    тема: функция React.PureComponent

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import { useState, memo } from 'react';
+import { useState, memo, PureComponent } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-function propsCompare(prevProps, nextProps) {
-  return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
-}
+// function propsCompare(prevProps, nextProps) {
+//   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
+// }
 
-const Form = memo((props) => {
-  console.log('render')
-  return (
-    <Container>
-      <form className="w-50 border mt-5 p-3 m-auto">
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-          <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-          <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-      </form>
-    </Container>
-  )
-}, propsCompare)
+// const Form = memo((props) => {
+//   console.log('render')
+//   return (
+//     <Container>
+//       <form className="w-50 border mt-5 p-3 m-auto">
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+//           <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//           <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+//         </div>
+//       </form>
+//     </Container>
+//   )
+// }, propsCompare)
+
+class Form extends PureComponent {
+  render() {
+    console.log('render')
+    return (
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+            <input value={this.props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </form>
+      </Container>
+    )
+  }
+}
 
 function App() {
   const [data, setData] = useState({
-    mail: {
-      name: "name@example.com"
-    },
+    mail: "name@example.com",
     text: 'some text'
   });
 
@@ -37,9 +55,7 @@ function App() {
       <Form mail={data.mail} text={data.text} />
       <button
         onClick={() => setData({
-          mail: {
-            name: "name@example.com"
-          },
+          mail: "ame@example.com",
           text: 'some text'
         })}>
         Click me
