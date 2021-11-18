e88b627d2033e11104be564b8dd56ac7ee846e51
Radik Abubjazov <abubjazov@mail.ru>
2021-11-18 18:28:04 +0300

    тема: React.memo + useCallBack

--- src/App.js
+++ src/App.js
@@ -1,83 +1,81 @@
-import { useState, memo, Component } from 'react';
-import { Container } from 'react-bootstrap';
-import './App.css';
+import { useState, memo, useCallback } from 'react'
+import { Container } from 'react-bootstrap'
+import './App.css'
 
 // function propsCompare(prevProps, nextProps) {
 //   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
 // }
 
-// const Form = memo((props) => {
-//   console.log('render')
-//   return (
-//     <Container>
-//       <form className="w-50 border mt-5 p-3 m-auto">
-//         <div className="mb-3">
-//           <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-//           <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-//           <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-//         </div>
-//       </form>
-//     </Container>
-//   )
-// }, propsCompare)
+const Form = memo((props) => {
+  console.log('render')
+  return (
+    <Container>
+      <form className="w-50 border mt-5 p-3 m-auto">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+          <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+          <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+    </Container>
+  )
+})
 
-class Form extends Component {
+// class Form extends Component {
 
-  shouldComponentUpdate(nextProps) {
-    if (this.props.mail.name === nextProps.mail.name) {
-      return false
-    }
+//   shouldComponentUpdate(nextProps) {
+//     if (this.props.mail.name === nextProps.mail.name) {
+//       return false
+//     }
 
-    return true
-  }
+//     return true
+//   }
 
-  render() {
-    console.log('render')
-    return (
-      <Container>
-        <form className="w-50 border mt-5 p-3 m-auto">
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-            <input value={this.props.mail.name} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-            <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-        </form>
-      </Container>
-    )
-  }
-}
+//   render() {
+//     console.log('render')
+//     return (
+//       <Container>
+//         <form className="w-50 border mt-5 p-3 m-auto">
+//           <div className="mb-3">
+//             <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+//             <input value={this.props.mail.name} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+//           </div>
+//           <div className="mb-3">
+//             <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//             <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+//           </div>
+//         </form>
+//       </Container>
+//     )
+//   }
+// }
 
 function App() {
   const [data, setData] = useState({
-    mail: {
-      name: "name@example.com"
-    },
+    mail: "name@example.com",
     text: 'some text'
-  });
+  })
+
+  const onLog = useCallback(() => console.log('WOW'), [])
 
   return (
     <>
-      <Form mail={data.mail} text={data.text} />
+      <Form mail={data.mail} text={data.text} onLog={onLog} />
       <button
         onClick={() => setData({
-          mail: {
-            name: "ame@example.com"
-          },
+          mail: "ame@example.com",
           text: 'some text'
         })}>
         Click me
       </button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
 
 
 
