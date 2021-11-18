afd99b081fef803a155551b92c3dd6a9edd28dbf
Radik Abubjazov <abubjazov@mail.ru>
2021-11-18 20:38:31 +0300

    тема: React.Context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { useState, memo, useCallback } from 'react'
+import { useState, Component, createContext } from 'react'
 import { Container } from 'react-bootstrap'
 import './App.css'
 
-// function propsCompare(prevProps, nextProps) {
-//   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
-// }
+const dataContext = createContext({
+  mail: "name@example.com",
+  text: 'some text'
+})
 
-const Form = memo((props) => {
+const { Provider, Consumer } = dataContext
+
+const Form = (props) => {
   console.log('render')
+
   return (
     <Container>
       <form className="w-50 border mt-5 p-3 m-auto">
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-          <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+          <InputComponent />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
@@ -22,7 +26,82 @@ const Form = memo((props) => {
       </form>
     </Container>
   )
-})
+}
+
+class InputComponent extends Component {
+  render() {
+    return (
+      <Consumer>
+        {
+          value => {
+            return (
+              <input
+                value={value.mail}
+                type="email"
+                className='form-control'
+                id="exampleFormControlInput1"
+                placeholder="name@example.com" />
+            )
+          }
+        }
+      </Consumer>
+    )
+  }
+}
+
+function App() {
+  const [data, setData] = useState({
+    mail: "name@example.com",
+    text: 'some text'
+  })
+
+  return (
+    <Provider value={data}>
+      <Form text={data.text} />
+      <button
+        onClick={() => setData({
+          mail: "ame@example.com",
+          text: 'some text 2'
+        })}>
+        Click me
+      </button>
+    </Provider>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+// import { useState, memo, useCallback } from 'react'
+// import { Container } from 'react-bootstrap'
+// import './App.css'
+
+// // function propsCompare(prevProps, nextProps) {
+// //   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
+// // }
+
+// const Form = memo((props) => {
+//   console.log('render')
+//   return (
+//     <Container>
+//       <form className="w-50 border mt-5 p-3 m-auto">
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+//           <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//           <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+//         </div>
+//       </form>
+//     </Container>
+//   )
+// })
 
 // class Form extends Component {
 
@@ -53,29 +132,29 @@ const Form = memo((props) => {
 //   }
 // }
 
-function App() {
-  const [data, setData] = useState({
-    mail: "name@example.com",
-    text: 'some text'
-  })
+// function App() {
+//   const [data, setData] = useState({
+//     mail: "name@example.com",
+//     text: 'some text'
+//   })
 
-  const onLog = useCallback(() => console.log('WOW'), [])
+//   const onLog = useCallback(() => console.log('WOW'), [])
 
-  return (
-    <>
-      <Form mail={data.mail} text={data.text} onLog={onLog} />
-      <button
-        onClick={() => setData({
-          mail: "ame@example.com",
-          text: 'some text'
-        })}>
-        Click me
-      </button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Form mail={data.mail} text={data.text} onLog={onLog} />
+//       <button
+//         onClick={() => setData({
+//           mail: "ame@example.com",
+//           text: 'some text'
+//         })}>
+//         Click me
+//       </button>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
 
 
 
