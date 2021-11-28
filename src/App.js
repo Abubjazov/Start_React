2458792cecc4937e06fc3bb7fc8ddeb638a22e90
Radik Abubjazov <abubjazov@mail.ru>
2021-11-29 00:05:13 +0300

    тема: Redux

--- src/App.js
+++ src/App.js
@@ -44,9 +44,11 @@ const { dispatch, subscribe, getState } = store
 const update = () => document.getElementById('counter').textContent = getState().value
 subscribe(update)
 
-const incDispatch = bindActionCreators(inc, dispatch)
-const decDispatch = bindActionCreators(dec, dispatch)
-const rndDispatch = bindActionCreators(rnd, dispatch)
+const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
+    incDispatch: inc,
+    decDispatch: dec,
+    rndDispatch: rnd
+}, dispatch)
 
 document.getElementById('inc').addEventListener('click', incDispatch)
 document.getElementById('dec').addEventListener('click', decDispatch)
